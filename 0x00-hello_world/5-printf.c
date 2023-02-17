c7156d2633f4889aafe60455e13f42e39f972c9c
smmrzr <dan.afidegnon@gmail.com>
2023-02-16 20:42:41 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -10,8 +9,7 @@
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome 
-			is a piece of art," "\n");
+        printf("with proper grammar, but the outcome is a piece of art," "\n");
 
         return (0);
 }
