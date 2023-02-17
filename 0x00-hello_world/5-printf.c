4018a5cee4c0139e1305b78c28ea1b10a3e6a3e3
smmrzr <dan.afidegnon@gmail.com>
2023-02-16 19:18:34 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
